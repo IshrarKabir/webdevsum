3e90af240ae4ff7e438f2491d96041f524ad30e1
IshrarKabir <ishrar.adrika91@myhunter.cuny.edu>
2024-06-18 06:34:48 -0400

    branch

---
+++ page3.js
@@ -0,0 +1,7 @@
+// function myFunction() {
+//     const element = document.getElementById("id");
+//     element.remove();
+// };
+function myremove() {
+    const myDiv = document.getElementById(image);
+    const parent= constant
