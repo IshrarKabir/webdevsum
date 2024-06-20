9fe878e69acf7575ad5048b0eeb7a6007def2343
IshrarKabir <ishrar.adrika91@myhunter.cuny.edu>
2024-06-20 16:31:26 -0400

    cat

--- page3.js
+++ page3.js
@@ -1,26 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var removeButton = document.getElementById('removeButton');
-    var addButton = document.getElementById('addButton');
-    var image1 = document.getElementById('image1');
-    var image2 = document.getElementById('image2');
-
-    removeButton.addEventListener('click', function() {
-        if (image1) {
-            image1.parentNode.removeChild(image1); 
+removeButton.onclick=function() {
+    var image1 = document.querySelector('#image1');
+    if (image1) {
+        image1.remove();
         }
-    });
+    };
+addButton.onclick = function() {
+    var addimage = document.createElement('img');
+    addimage.src = "https://upload.wikimedia.org/wikipedia/commons/0/04/So_happy_smiling_cat.jpg";
+    addimage.id = "image2";
+    addimage.alt = "A happy cat";
+    addimage.width = 400;
+    addimage.height = 400;
+    addButton.parentNode.appendChild(addimage);
+    };
 
-    var gatoimage = document.getElementById('image2');
-        if (!gatoimage) {
-            
-            var gato = document.createElement('img');
-            gato.src = "https://upload.wikimedia.org/wikipedia/commons/0/04/So_happy_smiling_cat.jpg";
-            gato.id = "image2";
-            gato.alt = "A happy cat";
-            gato.width = 400;
-            gato.height = 400;
-
-            addButton.parentNode.appendChild(gato); 
-        }
-    });
 
